55b285313da46adee953e05cccfc438c35fedbc9
Jay Yin <mumingv@163.com>
2016-03-17 23:40:25 +0800

    add some directory

---
+++ syntax/syntax_basic.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: Jie Yin
+@e-mail: mumingv@163.com
+@date:   2016-03-16
+""" 
+
+"""
+注释
+"""
+# 单行注释：使用#(井号)
+# 多行注释：使用一对'''(三个单引号)，或者使用一对"""(三个双引号)包含注释信息
+
+
+"""
+缩进
+"""
+# 使用缩进表示代码块，不用{}表示代码块
+
+
+"""
+变量
+"""
+# 命名
+## 1. 由字母、数字、下划线组成，且不能以数字开头；
+## 2. 中文字符也可以作为变量名称；
+
+# 保留字(共33个), 不能用作变量的名字
+## 8个: and as assert break class continue def del
+## 8个: elif else except finally for from globall if
+## 8个: import in is lambda nonlocal not or pass
+## 6个: raise return try while with yield
+## 3个: True False None
+
+
+"""
+函数
+"""
+
+
+
+"""
+类
+"""
+
+
+
+"""
+条件语句
+"""
+
+
+
+"""
+循环语句
+"""
+
+
+
+"""
+"""
+
