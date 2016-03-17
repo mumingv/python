a0bee588bcfc4fac2fe0e09a8fd0be3e36cf68f3
Jay Yin <mumingv@163.com>
2016-03-18 00:06:57 +0800

    add syntax

---
+++ syntax/syntax_string.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: Jie Yin
+@e-mail: mumingv@163.com
+@date:   2016-03-17
+""" 
+
+# 获取子字符串
+print "获取子字符串"
+str = "abcdefg"
+print str[0:2] #输出"ab", 表示区间[0,2)内的子字符串
+print str[-1] #输出"g", 表示下标为-1的字符
+print str[0:-1] #输出"abcdef", 表示除最后一个字符外的子字符串
+
+
