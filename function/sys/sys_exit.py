dddba6fcc552d70f92ba183e2a8f3e74e9559132
Jay Yin <mumingv@163.com>
2016-03-29 19:43:49 +0800

    add directory sys and urllib

---
+++ function/sys/sys_exit.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+'''
+/***********************************************************
+      FileName: sys_exit.py
+          Desc: 退出Python
+        Author: Jie Yin
+         Email: mumingv@163.com
+      HomePage: https://github.com/mumingv
+       Version: 0.0.1
+    LastChange: 2016-03-29 18:32:06
+       History:
+ ***********************************************************/
+'''
+
+# 语法：sys.exit([arg])
+# 说明：该方法会抛出一个 SystemExit 异常 
+
+# 示例
+import sys
+
+print "Hello"
+sys.exit(0)
+print "world"
+
---
+++ function/urllib/urllib_urlretrieve.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+'''
+/***********************************************************
+      FileName: urllib_urlretrieve.py
+          Desc: 将URL指定的资源拷贝到本地文件
+        Author: Jie Yin
+         Email: mumingv@163.com
+      HomePage: https://github.com/mumingv
+       Version: 0.0.1
+    LastChange: 2016-03-29 17:58:21
+       History:
+ ***********************************************************/
+'''
+
+# 语法：urllib.urlretrieve(url[, filename[, reporthook[, data]]])
+
+# 示例
+
+
+
+
+
+
+
+vlist = "Python is an excellent language".split()
+print vlist  #输出：['Python', 'is', 'an', 'excellent', 'language']
+str1 = "abcdefg"
+print str1.find("def")  #输出：3
+print str1.replace("c", "C")  #输出：abCdefg
+
