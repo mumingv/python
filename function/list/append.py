bdb55d845c958c8a9f576d0394b562c57993751e
mumingv <mumingv@163.com>
2016-09-01 15:35:29 +0800

    add file

---
+++ function/list/append.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+'''
+/***********************************************************
+      FileName: append.py
+          Desc: 将元素加在list末尾
+        Author: Jie Yin
+         Email: mumingv@163.com
+      HomePage: https://github.com/mumingv
+       Version: 0.0.1
+    LastChange: 2016-09-01 15:33:14
+       History:
+ ***********************************************************/
+'''
+
+# syntax: list.append(x)
+
+# 示例
+vlist = [0, 1, 2, 3, 4]
+print vlist  #输出：[0, 1, 2, 3, 4]
+vlist.append("python")
+print vlist  #输出：[0, 1, 2, 3, 4, 'python']
+
