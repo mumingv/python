e7edbbbab9edb920f1462c0fc86b2fdf8d2ec050
mumingv <mumingv@163.com>
2016-09-01 21:54:44 +0800

    add file

---
+++ function/numpy/tile.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+'''
+/***********************************************************
+      FileName: tile.py
+          Desc: 构造二维数组(元素可以是单个数值，也可以是一维数组或二维数组)
+        Author: Jie Yin
+         Email: mumingv@163.com
+      HomePage: https://github.com/mumingv
+       Version: 0.0.1
+    LastChange: 2016-09-01 21:52:23
+       History:
+ ***********************************************************/
+'''
+
+# syntax: tile()
+# http://scipy.github.io/old-wiki/pages/Numpy_Example_List.html#tile.28.29
+
+from numpy import tile
+
+# 
