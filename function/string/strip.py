2d366cee3f1d4a4281c107d8189d164afc7ad222
mumingv <mumingv@163.com>
2016-09-01 12:01:44 +0800

    add file

---
+++ function/string/strip.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+'''
+/***********************************************************
+      FileName: strip.py
+          Desc: 去除字符串开头和结尾的空白字符
+        Author: Jie Yin
+         Email: mumingv@163.com
+      HomePage: https://github.com/mumingv
+       Version: 0.0.1
+    LastChange: 2016-09-01 11:57:17
+       History:
+ ***********************************************************/
+'''
+
+# syntax: string.strip(s[, chars])
+
+# 示例
+str = "   Python is an excellent language".strip()
+print str  # "Python is an excellent language"
+
