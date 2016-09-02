34c33ccc18f1caffe78cf5ad17b2863719e3f162
mumingv <mumingv@163.com>
2016-09-02 16:52:17 +0800

    add file

--- function/numpy/tile.py
+++ function/numpy/tile.py
@@ -15,6 +15,38 @@
 # syntax: tile()
 # http://scipy.github.io/old-wiki/pages/Numpy_Example_List.html#tile.28.29
 
-from numpy import tile
+from numpy import *
 
-# 
+# 元素是单个数值
+v = 3
+a = tile(v, (3, 2))
+print(a)
+'''
+[[3 3]
+ [3 3]
+ [3 3]]
+'''
+
+# 元素是一维数组
+v = array([10, 20])
+a = tile(v, (3, 2))
+print(a)
+'''
+[[10 20 10 20]
+ [10 20 10 20]
+ [10 20 10 20]]
+'''
+
+# 元素是二维数组
+v = array([[10, 20], [30, 40]])
+a = tile(v, (3, 2))
+print(a)
+'''
+[[10 20 10 20]
+ [30 40 30 40]
+ [10 20 10 20]
+ [30 40 30 40]
+ [10 20 10 20]
+ [30 40 30 40]]
+'''
+
