ee520235cfb8bf7684500e6ed3b594f34690c5b6
mumingv <mumingv@163.com>
2017-03-14 10:55:14 +0800

    if

---
+++ syntax/if.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: Jie Yin
+@e-mail: mumingv@163.com
+@date:   2017-03-14
+""" 
+
+"""
+if...
+"""
+number = 3
+if number > 0:
+    print 'positive'
+
+"""
+if...else...
+"""
+number = -3
+if number > 0:
+    print 'positive'
+else:
+    print 'negative'
+
+"""
+if...elif...else
+"""
+number = 3
+if number < 0:
+    print 'negative'
+elif number < 10:
+    print 'positive little'
+else:
+    print 'positive big'
+
