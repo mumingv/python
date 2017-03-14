8157b52a566fce3a9e6dd9a76fc094ad769726c1
mumingv <mumingv@163.com>
2017-03-14 10:43:02 +0800

    loop

---
+++ syntax/loop.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: Jie Yin
+@e-mail: mumingv@163.com
+@date:   2017-03-14
+""" 
+
+"""
+for循环
+"""
+# 遍历字符串列表
+words = ['this', 'is', 'python', 'note']
+for word in words:
+    print word
+
+# 遍历数字列表
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+for number in numbers:
+    print number
+
+for number in range(0, 10):
+    print number
