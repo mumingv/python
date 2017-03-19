b2d3d0f8dea4b9e3bffbda05f9dcb29d38bdeab5
mumingv <mumingv@163.com>
2017-03-19 21:02:14 +0800

    python

---
+++ func/builtin/repr.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: Jie Yin
+@e-mail: mumingv@163.com
+@date:   2016-03-19
+""" 
+
+# repr - 返回表示对象的字符串
+# syntax: repr(object)
+# https://docs.python.org/2.7/library/functions.html#repr
+
+obj = (1, 2, 3)
+print repr(obj)
