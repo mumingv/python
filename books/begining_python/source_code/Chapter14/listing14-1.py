64b16e5b554ae1f3f6154386f8ec4a0a062ff356
mumingv <mumingv@163.com>
2017-05-29 22:20:50 +0800

    socket client

--- books/begining_python/source_code/Chapter14/listing14-1.py
+++ books/begining_python/source_code/Chapter14/listing14-1.py
@@ -29,7 +29,7 @@ s.bind((host, port))
 # 开始侦听
 s.listen(20)  # 实际队列中的最大连接数比配置的backlog要多一个, 如果backlog为1, 则客户端同时发送3个请求也能够处理
 while True:
-    # c是新建的socket对象，用于发送数据给客户端；addr是客户端的主机名(IP地址)和端口号
+    # c是新建的socket对象，用于发送数据给客户端；addr是客户端的IP地址和端口号
     c, addr = s.accept()
     print 'Got connection from', addr
     # 延迟5秒
--- books/begining_python/source_code/Chapter14/listing14-2.py
+++ books/begining_python/source_code/Chapter14/listing14-2.py
@@ -1,7 +1,8 @@
+# -*- coding: UTF-8 -*-
 '''
 /***********************************************************
       FileName: listing14-2.py
-          Desc: 
+          Desc: 客户端
         Author: Jie Yin
          Email: mumingv@163.com
       HomePage: https://github.com/mumingv
@@ -13,13 +14,28 @@
 import socket
 import time
 
+# 流程：sc (socket -> connect)
+
+# 获取Unix时间戳，用于计时
 begin = time.time()
+
+# 创建套接字, 默认参数为：AF_INET、SOCK_STREAM
 s = socket.socket()
 
+# 获取主机名称
 host = socket.gethostname()
+print host
 port = 1234
 
+# 连接到服务器的套接字
 s.connect((host, port))
-print s.recv(1024)
+
+# 接收数据
+while True:
+    string = s.recv(1024)
+    if len(string) > 0:
+        print string
+    else:
+        break
 end = time.time()
 print end - begin
