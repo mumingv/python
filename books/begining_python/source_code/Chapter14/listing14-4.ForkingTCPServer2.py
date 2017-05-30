5c3acc4e98baea47776267d40dd36ed467ea383c
mumingv <mumingv@163.com>
2017-05-30 12:50:10 +0800

    add

--- books/begining_python/source_code/Chapter14/listing14-4.ForkingTCPServer2.py
+++ books/begining_python/source_code/Chapter14/listing14-4.ForkingTCPServer2.py
@@ -2,7 +2,7 @@
 '''
 /***********************************************************
       FileName: listing14-4.py
-          Desc: 自定义ForkingTCPServer
+          Desc: 源生ForkingTCPServer
         Author: Jie Yin
          Email: mumingv@163.com
       HomePage: https://github.com/mumingv
@@ -11,12 +11,8 @@
        History:
  ***********************************************************/
 '''
-from SocketServer import TCPServer, ForkingMixIn, StreamRequestHandler
+from SocketServer import ForkingTCPServer, StreamRequestHandler
 import time
-
-# 该类等同于SocketServer库自带的ForkingTCPServer
-# ！注意：多重继承时，ForkingMixIn需要放在TCPServer前面
-class Server(ForkingMixIn, TCPServer): pass
 
 class Handler(StreamRequestHandler):
 
@@ -30,5 +26,5 @@ class Handler(StreamRequestHandler):
 
 # Server是一个ForkingTCPServer，所以对于每个客户端请求都会自动创建一个Handler实例
 # ForkingTCPServer是异步的，对于每个请求，服务端都会fork一个子进程来处理客户端请求，所以同时会有多个Handler实例在跑
-server = Server(('', 1234), Handler)
+server = ForkingTCPServer(('', 1234), Handler)
 server.serve_forever()
